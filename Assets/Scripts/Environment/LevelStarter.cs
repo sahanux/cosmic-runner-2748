44541528c7c6e8a5a10953421d559f3748d9dd7d
sahanux <sahanakalanka818@gmail.com>
2023-05-03 17:52:29 +0530

    sound fx added

--- Assets/Scripts/Environment/LevelStarter.cs
+++ Assets/Scripts/Environment/LevelStarter.cs
@@ -9,6 +9,9 @@ public class LevelStarter : MonoBehaviour
     public GameObject countDown1;
     public GameObject countDownGo;
     public GameObject fadeIn;
+    public AudioSource readyFX;
+    public AudioSource goFX;
+
     void Start()
     {
         StartCoroutine(CountSequence());
@@ -18,11 +21,15 @@ public class LevelStarter : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         countDown3.SetActive(true);
+        readyFX.Play();
         yield return new WaitForSeconds(1);
         countDown2.SetActive(true);
+        readyFX.Play();
         yield return new WaitForSeconds(1);
         countDown1.SetActive(true);
+        readyFX.Play();
         yield return new WaitForSeconds(1);
         countDownGo.SetActive(true);
+        goFX.Play();
     }
 }
