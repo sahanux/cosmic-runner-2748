876a3b27bdc330ea5f3745d3b7ab4ab2b94d7121
sahanux <sahanakalanka818@gmail.com>
2023-05-03 20:00:07 +0530

    fonts added

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,24 +6,29 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float leftRightSpeed = 4;
+    static public bool canMove = false;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if(canMove == true)
         {
-            if (this.gameObject.transform.position.x > LevelBoundry.leftSide){ //this line is for the level boundary. it means we cannot move beyond this the player
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
-            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (this.gameObject.transform.position.x > LevelBoundry.leftSide)
+                { //this line is for the level boundary. it means we cannot move beyond this the player
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                }
         }
          if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            if (this.gameObject.transform.position.x < LevelBoundry.rightSide){ //this line is for the level boundary. it means we cannot move beyond this the player
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            if (this.gameObject.transform.position.x < LevelBoundry.rightSide)
+            { //this line is for the level boundary. it means we cannot move beyond this the player
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
             }
 
+        }
         }
     }
 }
