a81c162d3cd30d2d0f9b321193c1e8003b15c78e
sahanux <sahanakalanka818@gmail.com>
2023-05-05 11:13:29 +0530

    crash sound has added

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -6,11 +6,13 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject chartModel;
+    public AudioSource crashThud;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         chartModel.GetComponent<Animator>().Play("Stumble Backwards");
+        crashThud.Play();
     }
 }
