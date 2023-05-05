66bdd0121ad2c7778ac200cdf70beea9ef20d907
sahanux <sahanakalanka818@gmail.com>
2023-05-05 22:49:30 +0530

    level end function created

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -8,13 +8,16 @@ public class ObstacleCollision : MonoBehaviour
     public GameObject chartModel;
     public AudioSource crashThud;
     public GameObject mainCam;
+    public GameObject levelControl;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         chartModel.GetComponent<Animator>().Play("Stumble Backwards");
+        levelControl.GetComponent<LevelDistance>().enabled = false;
         crashThud.Play();
         mainCam.GetComponent<Animator>().enabled = true;
+        levelControl.GetComponent<EndRunSequence>().enabled = true;
     }
 }
