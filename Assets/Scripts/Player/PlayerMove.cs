97c090079c072516423cbd3fc80f3c62cd6b1d7f
sahanux <sahanakalanka818@gmail.com>
2023-05-06 16:50:38 +0530

    Jump function added

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,6 +7,9 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 5;
     public float leftRightSpeed = 4;
     static public bool canMove = false;
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
 
     // Update is called once per frame
     void Update()
@@ -20,15 +23,45 @@ public class PlayerMove : MonoBehaviour
                 { //this line is for the level boundary. it means we cannot move beyond this the player
                     transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
                 }
-        }
-         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.gameObject.transform.position.x < LevelBoundry.rightSide)
-            { //this line is for the level boundary. it means we cannot move beyond this the player
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
             }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                if (this.gameObject.transform.position.x < LevelBoundry.rightSide)
+                { //this line is for the level boundary. it means we cannot move beyond this the player
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                }
 
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+            {
+                if (isJumping == false)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("Jump");
+                    StartCoroutine(JumpSequence());
+                }
+            }
         }
+        if(isJumping == true)
+        {
+            if(comingDown == false)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+            }
+             if(comingDown == true)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -3, Space.World);
+            }
         }
+    }
+
+    IEnumerator JumpSequence()
+    {
+       yield return new WaitForSeconds(0.45f);
+       comingDown = true; 
+       yield return new WaitForSeconds(0.45f);
+       isJumping = false;
+       comingDown = false;
+       playerObject.GetComponent<Animator>().Play("Standard Run");
     }
 }
